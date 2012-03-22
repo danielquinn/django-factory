f55eadde1b9fffbeb4bcc3b71dd0161a51ad27b0
Daniel Quinn <code@danielquinn.org>
2012-03-22 17:07:43 +0100

    Added acquire_object_or_404() and moved the FactoryManager into it's own file

---
+++ src/django_factory/managers.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+class FactoryManager(models.Manager):
+	"""
+		Custom manager to handle object fetching for subclasses of Factory
+	"""
+
+	def acquire(self, **kwa):
+		return self._get_leaf_class(**kwa).objects.get(**kwa)
+
+
+	def acquire_object_or_404(self, **kwa):
+		from django.shortcuts import get_object_or_404
+		return get_object_or_404(self._get_leaf_class(**kwa), **kwa)
+
+
+	def _get_leaf_class(self, **kwa):
+		"""
+			If the lookup in question belongs to a specific class, return that
+			class, otherwise just return the class this manager is managing.
+		"""
+
+		from django.db import connection
+
+		where_selector, where_val = kwa.items()[0]
+
+		root = self._get_root() # The top-level class from which all factory methods were spawned
+		qn = connection.ops.quote_name
+
+		if where_selector == "pk":
+			where_selector = root.pk.column
+
+		query = """
+			SELECT
+				_factorypath
+			FROM
+				%(factory)s
+			WHERE
+				%(where_selector)s = %%s
+		""" % {
+			"where_selector": "%s.%s" % (root.db_table, qn(where_selector)),
+			"factory": qn(root.db_table),
+			}
+
+		cursor = connection.cursor()
+		cursor.execute(query, (where_val,))
+		result = cursor.fetchone()
+		if result and self.model.get_factorypaths():
+			return self.model.get_factorypaths()[result[0]]
+
+		return self.model
+
+
+	def _get_root(self):
+
+		from django_factory.models import Factory
+
+		t = self.model
+		while Factory not in t.__bases__:
+			t = t.__base__
+
+		return t._meta
--- src/django_factory/models.py
+++ src/django_factory/models.py
@@ -1,116 +1,72 @@
-# -*- coding: utf-8 -*-
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
-class FactoryManager(models.Manager):
-    """
-        Custom manager to handle object fetching for subclasses of Factory
-    """
-
-    def acquire(self, **kwa):
-
-        where_selector, where_val = kwa.items()[0]
-
-        from django.db import connection
-
-        root = self._get_root() # The top-level class from which all factory methods were spawned
-        qn = connection.ops.quote_name
-
-        if where_selector == "pk":
-            where_selector = root.pk.column
-
-        query = """
-            SELECT
-                _factorypath
-            FROM
-                %(factory)s
-            WHERE
-                %(where_selector)s = %%s
-        """ % {
-            "where_selector": "%s.%s" % (root.db_table, qn(where_selector)),
-            "factory": qn(root.db_table),
-        }
-
-        cursor = connection.cursor()
-        cursor.execute(query, (where_val,))
-        result = cursor.fetchone()
-        if result and self.model.get_factorypaths():
-            cls = self.model.get_factorypaths()[result[0]]
-            return cls.objects.get(**kwa)
-
-        return self.get(**kwa)
-
-
-    def _get_root(self):
-        t = self.model
-        while Factory not in t.__bases__:
-            t = t.__base__
-        return t._meta
-
+from django_factory.managers import FactoryManager
 
 class Factory(models.Model):
-    """
-        Class factory abstract parent class.
-        Subclass this to create your own factory
-    """
+	"""
+		Class factory abstract parent class.
+		Subclass this to create your own factory
+	"""
 
-    class Meta:
-        abstract = True
-
-
-    _paths = {} # Cache of paths to classes that subclass the parent factory
-
-    _factorypath = models.CharField(max_length=255,db_index=True)
-
-    objects = FactoryManager()
-
-    def save(self, *a, **kwa):
-        if not self._factorypath:
-            self._factorypath = self.get_factorypath()
-        super(Factory, self).save(*a, **kwa)
+	class Meta:
+		abstract = True
 
 
-    def get_meta(self):
-        return self.__class__.objects.acquire(pk=self.pk)
+	_paths = {} # Cache of paths to classes that subclass the parent factory
+
+	_factorypath = models.CharField(max_length=255,db_index=True)
+
+	objects = FactoryManager()
+
+	def save(self, *a, **kwa):
+		if not self._factorypath:
+			self._factorypath = self.get_factorypath()
+		super(Factory, self).save(*a, **kwa)
 
 
-    @classmethod
-    def get_factorypath(cls):
-        """
-            Return the import path to this class.  We use this as the
-            identifier for when the factory creates or acquires an instance of
-            this class.
-        """
-        return cls.__module__ + "." + cls.__name__
+	def get_meta(self):
+		"""
+			Handy to run when you want to get all the properties of an object, not just its root information.
+		"""
+		return self.__class__.objects.acquire(pk=self.pk)
 
 
-    @classmethod
-    def get_factorypaths(cls):
-        """
-            Return a list of all classes that subclass this factory.
-        """
-
-        if not cls._paths:
-            cls._set_factorypaths()
-
-        return cls._paths
+	@classmethod
+	def get_factorypath(cls):
+		"""
+			Return the import path to this class.  We use this as the
+			identifier for when the factory creates or acquires an instance of
+			this class.
+		"""
+		return cls.__module__ + "." + cls.__name__
 
 
-    @classmethod
-    def _set_factorypaths(cls):
-        """
-            Compiles a list of python paths to objects subclassed from this
-            model which we then use to determine the model to instantiate for
-            the data requested.
-        """
+	@classmethod
+	def get_factorypaths(cls):
+		"""
+			Return a list of all classes that subclass this factory.
+		"""
 
-        paths = {} # We don't use cls._paths because we recurse below
-        for c in cls.__subclasses__():
-            paths[c.get_factorypath()] = c
+		if not cls._paths:
+			cls._set_factorypaths()
 
-            # Recursion!
-            if c.__subclasses__():
-                paths.update(c.get_factorypaths())
+		return cls._paths
 
-        cls._paths = paths
+
+	@classmethod
+	def _set_factorypaths(cls):
+		"""
+			Compiles a list of python paths to objects subclassed from this
+			model which we then use to determine the model to instantiate for
+			the data requested.
+		"""
+
+		paths = {} # We don't use cls._paths because we recurse below
+		for c in cls.__subclasses__():
+			paths[c.get_factorypath()] = c
+
+			# Recursion!
+			if c.__subclasses__():
+				paths.update(c.get_factorypaths())
+
+		cls._paths = paths
 
